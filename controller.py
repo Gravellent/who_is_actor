d17286bfc5c5f182e525def850ad316b60bc9b58
Zhi Qi <zq2175@columbia.edu>
2022-04-15 14:43:52 -0700

    major voting system updates
    1. new voting display message and collapsible voting details
    2. winning now can see ally team actor before enemy team finishes voting
    3. add 称号 in items
    4. remove team1 win and team2 win button, 结束游戏 button now grabs data from api
    5. some cosmetic updates

--- controller.py
+++ controller.py
@@ -91,12 +91,19 @@ def populate_game_data_from_history(game_id):
                 print(team100, team200)
                 if (set(team100) == set(item['team1']) and set(team200) == set(item['team2'])) or (set(team100) == set(item['team2']) and set(team200) == set(item['team1'])):
                     item['match_id'] = match_id
+                    if game['info']['teams'][0]['win']:
+                        item['winning_team'] = 'team1'
+                        item['votes'] = [None for _ in range(len(item['team1']))]
+                    else:
+                        item['winning_team'] = 'team2'
+                        item['votes'] = [None for _ in range(len(item['team2']))]
                     dynamo.tables['actor_game'].put_item(Item=item)
                     update_game_info(game_id, match_id)
+                    calculate_winning_score(game_id)
                     print('Updated:', game_id)
-                    return
+                    return True
     except:
-        return None
+        return False
 
 def get_profile_from_name(summoner_name):
     return requests.get(
@@ -107,29 +114,34 @@ def get_match_history_from_puuid(puuid):
     return requests.get(
     'https://riot.iesdev.com/graphql?query=query%20LeagueMatchlist($region:Region!,$puuid:ID!){matchlist(region:$region,puuid:$puuid){matches{id%20playerMatch{id%20playerMatchStats{lp}}}}}&variables={%22region%22:%22NA1%22,%22puuid%22:%22' + puuid +'%22}').json()
 
-def calculate_score(game_id):
-    
+def calculate_winning_score(game_id):
     item = dynamo.tables['actor_game'].get_item(Key={'game_id': game_id})['Item']
-    # calculate for winning team
     winning_team = item['winning_team']
     kda = []
+    
     for idx, summoner_name in enumerate(item[winning_team]):
         item['participants'][summoner_name]['score'] = 1 # default 分数是1
         kda.append(item['participants'][summoner_name]['kda'])
-    max_kda_idx = np.argmax(kda)
+    max_kda_idx = np.argmax(kda).item()
+    item['winning_team_max_kda_idx'] = max_kda_idx
     if max_kda_idx == item[winning_team+'_actor_idx']: #演员kda最高
         item['participants'][item[winning_team][max_kda_idx]]['score'] = 0
     else: #演员 kda不是最高
         item['participants'][item[winning_team][max_kda_idx]]['score'] = 2 #kda最高 +2
         item['participants'][item[winning_team][int(item[winning_team+'_actor_idx'])]]['score'] = -1 #演员 -1
+    dynamo.tables['actor_game'].put_item(Item=item)
     
-    # calculate for losing team
+def calculate_losing_score(game_id):
+    item = dynamo.tables['actor_game'].get_item(Key={'game_id': game_id})['Item']
+    item['chenghao'] = []
     losing_team = 'team1' if item['winning_team'] == 'team2' else 'team2'
     kda = []
+    
     for summoner_name in item[losing_team]:
         item['participants'][summoner_name]['score'] = -1 # default 分数是-1
         kda.append(item['participants'][summoner_name]['kda'])
-    max_kda_idx = np.argmax(kda)
+    max_kda_idx = np.argmax(kda).item()
+    item['losing_team_max_kda_idx'] = max_kda_idx
     
     # count votes
     vote_received = [0] * len(item[losing_team])
@@ -139,11 +151,15 @@ def calculate_score(game_id):
     zhuojianzaichuang = int(item[losing_team+'_actor_idx']) == np.argmax(vote_received)
     
     # 投对了平民 +0，演员 -3
-    if zhuojianzaichuang:
+    if zhuojianzaichuang: # 捉奸在床
         for idx, summoner_name in enumerate(item[losing_team]):
             item['participants'][summoner_name]['score'] = 0 if idx != item[losing_team+'_actor_idx'] else -3
-    else:
-        # 【蒙混过关】+3, 【运筹帷幄】+5
-        item['participants'][item[losing_team][int(item[losing_team+'_actor_idx'])]]['score'] = 3 if max_kda_idx  != item[losing_team+'_actor_idx'] else 5
+        item['chenghao'].append('zjzc')
+    elif max_kda_idx  != int(item[losing_team+'_actor_idx']): #蒙混过关
+        item['participants'][item[losing_team][int(item[losing_team+'_actor_idx'])]]['score'] = 3
+        item['chenghao'].append('mhgg')
+    else: # 运筹帷幄
+        item['participants'][item[losing_team][int(item[losing_team+'_actor_idx'])]]['score'] = 3
+        item['chenghao'].append('ycww')
     dynamo.tables['actor_game'].put_item(Item=item)
     
